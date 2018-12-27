da43ff44455e9f89a06032193cd3a0ffdff7f31c
S M <VS6993@zebra.com>
2018-12-27 18:39:23 +0530

    integet to binary progrma

--- integer_to_binary.py
+++ integer_to_binary.py
@@ -12,18 +12,27 @@ def int_to_bin(integer):
          #   bin=integer%2
           #  r=r+bin*10
           #  integer=int(integer/2)"""
-        while integer > 0:
+        while integer > 0:                         # actual code is here
             if integer & 1:
-              bin=bin+str(1)
+              bin=str(1)+bin
             else:
-                bin=bin+str(0)
+              bin=str(0)+bin
             integer=integer >> 1
 
-    print(bin)
+    return bin
 
 
 integer = int(input("enter a number"))
 
-int_to_bin(integer)
+
+z=int_to_bin(integer)
+print(z)
+try:
+  f=open('binary.txt','w')            # simply opening file and writing binary vales into it
+  f.write(z)
+except IOError("problem in opening file") as e:
+    print(e)
+finally:
+    f.close()
 
 
