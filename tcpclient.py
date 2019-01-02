0630c6bf2461f0692f66ed88b7a15919066b56b3
S M <VS6993@zebra.com>
2019-01-02 16:57:25 +0530

    Tcp client side program

---
+++ tcpclient.py
@@ -0,0 +1,22 @@
+import socket
+
+
+def main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host,port))
+
+    message=raw_input("->")
+
+
+    while message != 'q':
+        s.send(message)
+        data=s.recv(1024)
+        print("receibved from server:"+str(addr))
+        message=raw_input("->")
+    s.close()
+
+if __name__ == '__main__':
+    main()
